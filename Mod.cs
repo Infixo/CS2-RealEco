e9b72d42e65776fde2a0952d3ea5d1aa1bca6a5f
Infixo <swidwins@gmail.com>
2024-03-28 19:54:14 +0100

    Official project template

--- Mod.cs
+++ Mod.cs
@@ -1,28 +1,37 @@
-﻿using Colossal.Logging;
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
 using Game;
 using Game.Modding;
 using Game.SceneFlow;
 
-namespace RealEco
+namespace RealEco;
+
+public class Mod : IMod
 {
+    public static ILog log = LogManager.GetLogger($"{nameof(RealEco)}").SetShowsErrorsInUI(false);
+    private Setting m_Setting;
 
-    public class Mod : IMod
+    public void OnLoad(UpdateSystem updateSystem)
     {
-        public static ILog log = LogManager.GetLogger($"{nameof(RealEco)}").SetShowsErrorsInUI(false);
+        log.Info(nameof(OnLoad));
 
-        public void OnLoad(UpdateSystem updateSystem)
+        if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+            log.Info($"Current mod asset at {asset.path}");
+
+        m_Setting = new Setting(this);
+        m_Setting.RegisterInOptionsUI();
+        GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+        AssetDatabase.global.LoadSettings(nameof(RealEco), m_Setting, new Setting(this));
+    }
+
+    public void OnDispose()
+    {
+        log.Info(nameof(OnDispose));
+        if (m_Setting != null)
         {
-            log.Info(nameof(OnLoad));
-
-            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
-                log.Info($"Current mod asset at {asset.path}");
-
-            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN());
-        }
-
-        public void OnDispose()
-        {
-            log.Info(nameof(OnDispose));
+            m_Setting.UnregisterInOptionsUI();
+            m_Setting = null;
         }
     }
 }
